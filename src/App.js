fd59b5c2869a284fc7134b1ea71c33debb292104
Haris Dzulkifli <haris.dzulkifli@u.nus.edu>
2022-06-01 23:03:30 +0800

    Attempted to create Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 import "./App.css";
 import { Routes, Route } from 'react-router-dom'
-import { Container, Row, Col} from 'react-bootstrap'
+import { Container, Row, Col, Navbar} from 'react-bootstrap'
 import Login from "./Components/Login";
 import Signup from "./Components/Signup"
 import Home from "./Components/Home"
 import { UserAuthContextProvider } from "./Context/UserAuthContext";
 import ProtectedRoute from "./Components/ProtectedRoute";
+import PMS from "./Components/Pms";
+import SMS from "./Components/Sms";
 
 function App() {
   return (
@@ -19,6 +21,8 @@ function App() {
           <UserAuthContextProvider>
             <Routes>
               <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+              <Route path="/Sms" element={<ProtectedRoute><SMS /></ProtectedRoute>} />
+              <Route path="/Pms" element={<ProtectedRoute><PMS /></ProtectedRoute>} />
               <Route path="/" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
             </Routes>    
@@ -27,6 +31,7 @@ function App() {
         </Col>
       </Row>
   </Container>
+
 
   )
 }
